8d68b3e74c480955e75b214cbe0e7b058631318e
Lyubimov <rellitsabul@mail.ru>
2022-12-20 00:24:04 +0300

    Initial commit

---
+++ src/main/java/web/service/UserServiceImp.java
@@ -0,0 +1,34 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.model.User;
+import java.util.List;
+
+@Component
+@Transactional(readOnly = false)
+public class UserServiceImp implements UserService {
+
+    private final UserDao userDao;
+    @Autowired
+    public UserServiceImp(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    public List<User> listAll() {
+        return userDao.listAll();
+    }
+    public void delete(int id) {
+        userDao.delete(id);
+    }
+    public void save(User user) {
+        userDao.save(user);
+    }
+    public void update(int id, User updatedUser) { userDao.update(id, updatedUser);}
+    public User show(int id) {
+       return userDao.show(id);
+    }
+
+}
